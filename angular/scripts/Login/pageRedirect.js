71b1fef55786c7c1ddb8cf97f5ff0bcc57680127
mugeshkumarrepo <mugeshkumars@deemsysinc.com>
2015-09-11 17:26:35 +0530

    remove unwanted files

--- angular/scripts/Login/pageRedirect.js
+++
@@ -1,9 +0,0 @@
-var pageRedirect = angular.module('indexApp', []);
-
-pageRedirect.controller("affiliateApply",function($scope){
-    $scope.redirect="affiliate_login";
-});
-
-pageRedirect.controller("userApply",function($scope){
-    $scope.redirect="login";
-});
