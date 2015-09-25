7d8425236a18eaf359525910fdf065882c6cfcb1
mugeshkumarrepo <mugeshkumars@deemsysinc.com>
2015-09-25 17:02:06 +0530

    testimonial design changes with browser compatibility

--- js/script.js
+++ js/script.js
@@ -176,15 +176,13 @@ $(document).ready(function ($) {
       start();
     }
 	
-	
-	
 	////------- Projects Carousel
 	$(".projects-carousel").owlCarousel({
 		navigation : true,
 		pagination: false,
 		slideSpeed : 400,
 		stopOnHover: true,
-    	autoPlay: 3000,
+    	autoPlay: false,
     	items : 4,
     	itemsDesktopSmall : [900,3],
 		itemsTablet: [600,2],
@@ -204,10 +202,6 @@ $(document).ready(function ($) {
 		autoHeight : true,
 		transitionStyle : "fade"
 	});
-	
-	
-	
-	
 	
 	
 	////------- Custom Carousel
