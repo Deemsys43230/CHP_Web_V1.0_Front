fc31f6ffce68b7e2e8994a03d29ac6c207c0da35
storagerepo <deemsysinchr@gmail.com>
2015-10-17 19:57:00 +0530

    Commit for user end food diary.

--- app/userDashboard/userDashboardController.js
+++ app/userDashboard/userDashboardController.js
@@ -1,45 +1,49 @@
 var userApp= angular.module('userApp', ['ngRoute','oc.lazyLoad','ngCookies','requestModule','flash','ngAnimate','ngTouch', 'angucomplete-alt']);
 
 userApp.controller('UserDashboardController',function($scope,requestHandler,Flash) {
-    $scope.foodSearchResult = [{
-        "status": 1,
-        "foodimage": "http://192.168.1.71/CyberHealthMedia/FoodImages/13/1000x1000.jpg",
-        "foodImagePath": "http://192.168.1.71/CyberHealthMedia/FoodImages/13/50x50.jpg",
-        "foodid": 13,
-        "sessionid": [
-            {
-                "sessionid": 1,
-                "sessionname": "Breakfast"
-            },
-            {
-                "sessionid": 2,
-                "sessionname": "Brunch"
-            },
-            {
-                "sessionid": 3,
-                "sessionname": "Lunch"
-            }
-        ],
-        "foodname": "Delhi poori",
-        "categoryid": [
-            {
-                "categoryid": 4,
-                "categoryname": "Sea Food"
-            },
-            {
-                "categoryid": 5,
-                "categoryname": "Spicy Fries"
-            },
-            {
-                "categoryid": 8,
-                "categoryname": "Roast Spices"
-            }
-        ]
-    }];
+    $scope.foodSearchResult = [];
 
     $scope.foodSelected=function(selected){
-      alert(selected);
+        requestHandler.postRequest("user/getFoodDetailByUser/",{"foodid":selected.description.foodid}).then(function (response) {
+            $scope.userSelectedFoodDetails=response.data.Food_Data;
+
+            $.each($scope.userSelectedFoodDetails, function(index,value){
+                value.foodImagePath=value.foodImagePath+"200x200.jpg";
+            });
+
+            console.log($scope.userSelectedFoodDetails);
+
+        }, function () {
+            errorMessage(Flash, "Please try again later!")
+        });
     };
+
+    $scope.getFoodDiary=function(){
+
+        requestHandler.postRequest("user/getFoodbyDate/",{"addeddate":"17/10/2015"}).then(function (response) {
+            $scope.userFoodDiaryDataAll=response.data.MyFoodData;
+            $scope.getBreakfast();
+        }, function () {
+            errorMessage(Flash, "Please try again later!")
+        });
+    };
+
+    $scope.getBreakfast=function(){
+           $scope.userFoodDiaryData=$scope.userFoodDiaryDataAll.BreakFast;
+        $.each($scope.userFoodDiaryData, function(index,value){
+            value.foodImagePath=value.foodImagePath.substring(value.foodImagePath.indexOf("/") + 14,value.foodImagePath.length)+"200x200.jpg";
+        });
+
+    }
+
+
+    $scope.getBrunch=function(){
+        $scope.userFoodDiaryData=$scope.userFoodDiaryDataAll.Brunch;
+        $.each($scope.userFoodDiaryData, function(index,value){
+            value.foodImagePath=value.foodImagePath.substring(value.foodImagePath.indexOf("/") + 14,value.foodImagePath.length)+"200x200.jpg";
+        });
+
+    }
 
      $scope.inputChanged = function(str) {
         requestHandler.postRequest("searchFoodListByUser/",{"foodname":str}).then(function (response) {
@@ -50,12 +54,11 @@ userApp.controller('UserDashboardController',function($scope,requestHandler,Flas
             });
 
             $scope.foodSearchResult=$scope.searchResponse;
-
-
-
         }, function () {
             errorMessage(Flash, "Please try again later!")
         });
     }
+
+    $scope.getFoodDiary();
 
 });
