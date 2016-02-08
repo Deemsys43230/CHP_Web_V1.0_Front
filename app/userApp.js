a7033d7f49e9b9ca6b053d0e1bdebbf0a9bac56e
vinorepo <vinodhinib@deemsysinc.net>
2016-02-08 11:17:55 +0530

    paypal email validation done for user interface

--- app/userApp.js
+++ app/userApp.js
@@ -787,3 +787,23 @@ userApp.directive('block', function () {
         }
     }
 });
+
+//Check For Email Validation
+userApp.directive('validateEmail', function() {
+    var EMAIL_REGEXP = /^[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,5})$/;
+
+    return {
+        require: 'ngModel',
+        restrict: '',
+        link: function(scope, elm, attrs, ctrl) {
+            // only apply the validator if ngModel is present and Angular has added the email validator
+            if (ctrl && ctrl.$validators.email) {
+
+                // this will overwrite the default Angular email validator
+                ctrl.$validators.email = function(modelValue) {
+                    return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                };
+            }
+        }
+    };
+});
