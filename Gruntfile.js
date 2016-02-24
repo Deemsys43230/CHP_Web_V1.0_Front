c69cc3cc1d10440f7e4b95c4e63a64b8b7ee02ff
storagerepo <deemsysinchr@gmail.com>
2016-02-24 13:25:36 +0530

    Commit for grunt watch - task runner.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,27 @@
 module.exports=function(grunt){
-  grunt.initConfig({
-      uglify: {
-          min: {
-              files: grunt.file.expandMapping('app/**/*.js', 'CyberHealth/', {
-                  rename: function(destBase, destPath) {
-                      return destBase+destPath.replace('.js', '.js');
-                  }
-              })
-          }
-      }
-  });
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.initConfig({
+        watch: {
+        	options: { nospawn: true },
+            scripts: {
+                files: ['app/*.js','app/**/*.js'],
+                tasks: ['newer:uglify']
+            }
+        },
+        uglify  : {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['app/*.js','app/**/*.js'],
+                        dest: 'CyberHealth/',
+                        ext: '.js'
+                    }
+                ]
+            }
+        }
+    });
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.registerTask('default', ['newer:uglify', 'watch']);
 };
