d86b6b3e4e682a6e27822a37ac626c33702e39fe
vinorepo <vinodhinib@deemsysinc.net>
2016-02-24 18:38:45 +0530

    commit after minifying images using grunt imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,11 +27,22 @@ module.exports=function(grunt){
               src: ['angular/angular-cookies.js', 'angular/ocLazyLoad.min.js', 'angular/angular-animate-1.4.3.js','angular/angular-flash.min.js','js/jquery.migrate.js','js/jquery.fitvids.js','js/nivo-lightbox.min.js','js/owl.carousel.min.js','js/jquery.isotope.min.js','js/jquery.appear.js','js/count-to.js','js/jquery.textillate.js','js/jquery.lettering.js','js/jquery.easypiechart.min.js','plugin/scroll/jquery.nicescroll.min.js','js/jquery.parallax.js','js/mediaelement-and-player.js','angular/angular-touch.js','plugin/dateRange/moment.js','angular/roundProgress.js','angular/ui-bootstrap-tpls-0.14.0.js','js/prefixfree.min.js','plugin/highchart/highchart.js','plugin/highchart/highchart-more.js','angular/angular-count-to.min.js','angular/angular-nicescroll.js'],
               dest: 'CyberHealth/angular/output.js'
            }
-       }
+       },
+       imagemin: {                         // Task
+          dynamic: {                         // Another target
+               files: [{
+                   expand: true,                  // Enable dynamic expansion
+                          // Src matches are relative to this path
+                   src: ['images/*.{png,jpg,gif}','images/**/*.{png,jpg,gif}'],   // Actual patterns to match
+                   dest: 'CyberHealth/'                  // Destination path prefix
+               }]
+           }
+   }
    });
 	grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-newer');
     grunt.registerTask('default', ['newer:uglify', 'watch']);
 };
