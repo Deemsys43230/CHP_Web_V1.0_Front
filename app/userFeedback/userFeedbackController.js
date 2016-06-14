2aed02bed0759d290f488ea683bfa154c46daafb
vinorepo <vinodhinib@deemsysinc.net>
2016-06-14 20:43:31 +0530

    Commit for feedbacks loading.

--- app/userFeedback/userFeedbackController.js
+++ app/userFeedback/userFeedbackController.js
@@ -2,16 +2,57 @@ var adminApp = angular.module('adminApp', ['ngRoute','oc.lazyLoad','requestModul
 
 adminApp.controller('UserFeedbackController',['$scope','requestHandler','Flash','$routeParams',function($scope,requestHandler,Flash,$routeParams) {
 
+    $scope.currentPage=1;
     $scope.getUserFeedBackDetails= function(){
 
-        $scope.userFeedbackList="";
+        $scope.loaded=true;
+        $scope.offset=($scope.currentPage-1)*$scope.itemsPerPage;
+        $scope.limit=$scope.currentPage*$scope.itemsPerPage;
+
+        $scope.params={
+            "limit":$scope.limit,
+            "offset":$scope.offset,
+            "search":$scope.searchText
+        };
+
+        requestHandler.postRequest("admin/getFeedback/", $scope.params).then(function(response){
+            $scope.userFeedbackList=response.data.feedbackList;
+            $scope.total_count=response.data.totalrecords;
+            $scope.loaded=false;
+        });
     };
 
     $scope.removeUserFeedBackDetails=function(){
 
     };
+    $scope.feedbackid=[];
+    $scope.isChecked=function(){
+        $scope.getUserFeedBackDetails();
+        alert("hi");
+        console.log($scope.userFeedbackList);
+        console.log($scope.total_count);
+        if($scope.total_count>0){
+            alert("hello");
+            $.each($scope.userFeedbackList, function(index,value) {
+                if(document.getElementById('checkAll').checked==true){
+                    $scope.feedbackid.push(value.feedbackid);
+                    $("#"+value.feedbackid).checked==true;
+                    //$("#"+value.id).prop('checked',$(this).prop("checked"));
+
+                }
+                else if(document.getElementById('checkAll').checked==false){
+                    $scope.feedbackid=[];
+                    //$("#"+value.id).prop('checked', $(this).prop("checked"));
+                    $("#"+value.feedbackid).checked==false;
+                }
+            });
+
+        }
+    };
 
     $scope.init=function(){
+        $scope.itemsPerPage = 10;
+        $scope.searchText=" ";
         $scope.getUserFeedBackDetails();
     };
 
