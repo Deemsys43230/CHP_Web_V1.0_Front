fd78cafb84a4c55c7262b6f56d1459cc56de382b
Amalasrepo <amalas@deemsysinc.com>
2017-11-17 12:40:01 +0530

    commit with after user session out,Bmi graph issue in home page....

--- app/commonApp.js
+++ app/commonApp.js
@@ -74,6 +74,7 @@ commonApp.config(['$routeProvider','$ocLazyLoadProvider','$httpProvider',
                             name:'commonApp',
                             files:[
                                 'plugin/vertical-carousel/vertical-carousel.js',
+                                'css/stepProgressBar.css',
                                 'app/commonController.js'
                             ]
                         })
